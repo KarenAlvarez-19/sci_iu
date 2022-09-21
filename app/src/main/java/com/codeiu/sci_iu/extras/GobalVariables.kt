730e2d048c48e933430f9691d88f29ce665922d2
Misael Villegas <mizza@Misaels-MacBook-Pro.local>
2022-09-20 20:17:35 -0500

    init

---
+++ app/src/main/java/com/codeiu/sci_iu/extras/GobalVariables.kt
@@ -0,0 +1,10 @@
+package com.codeiu.sci_iu.extras
+
+class GobalVariables {
+    companion object {
+        var url_api = "http://192.168.0.104:8085/"
+//        var url_api = "http://192.168.43.155:8085/"
+        var url_login = url_api + "api/login"
+
+    }
+}
---
+++ app/src/main/java/com/codeiu/sci_iu/extras/Models.kt
@@ -0,0 +1,21 @@
+package com.codeiu.sci_iu.extras
+
+import com.google.gson.annotations.SerializedName
+
+class Models {
+        data class Data(
+            @SerializedName("token")
+            var token: String? = null,
+            @SerializedName("name")
+            var name: String? = null
+            )
+
+        data class LoginResponse (
+            @SerializedName("success")
+            var success: Boolean? = null,
+            @SerializedName("message")
+            var message: String? = null,
+            @SerializedName("data")
+            var data: Data? = null,
+        )
+}
